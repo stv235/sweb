bc650edd12a8a5672bbf0bf321e2174f7e5a8c0c
Steffen Hempel <git@m.krabw.de>
2020-04-23 12:51:47 +0200

    added recursive template loading

--- template.go
+++ template.go
@@ -1,20 +1,57 @@
 package sweb
 
 import (
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
+	"os"
 	"path/filepath"
 )
 
 func LoadTemplate(root string) *template.Template {
 	log.Println("[TEMPLATE]", "loading templates from", root)
 
-	html := template.New("")
+	//found := false
 
-	found := false
+	var html *template.Template
 
+	if err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			buf, err := ioutil.ReadFile(path)
+
+			if err != nil {
+				return err
+			}
+
+			rel, err := filepath.Rel(root, path)
+
+			if err != nil {
+				return err
+			}
+
+			name := filepath.ToSlash(rel)
+
+			if html == nil {
+				html = template.New(name)
+			} else {
+				html = html.New(name)
+			}
+
+			if _, err := html.Parse(string(buf)); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}); err != nil {
+		log.Fatalln(err)
+	}
+
+	/*
 	if files, err := ioutil.ReadDir(root); err == nil {
 		for _, file := range files {
 			if !file.IsDir() {
@@ -34,7 +71,7 @@ func LoadTemplate(root string) *template.Template {
 	if !found {
 		err := fmt.Errorf ("no templates found in folder %v", root)
 		log.Fatalln(err)
-	}
+	}*/
 
 	return html
 }
