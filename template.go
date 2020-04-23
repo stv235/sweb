363c79b2846306837d9ca87b66de24938be93a6e
Steffen Hempel <git@m.krabw.de>
2020-04-23 12:53:14 +0200

    template.go cleanup

--- template.go
+++ template.go
@@ -11,8 +11,6 @@ import (
 func LoadTemplate(root string) *template.Template {
 	log.Println("[TEMPLATE]", "loading templates from", root)
 
-	//found := false
-
 	var html *template.Template
 
 	if err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
@@ -50,28 +48,6 @@ func LoadTemplate(root string) *template.Template {
 	}); err != nil {
 		log.Fatalln(err)
 	}
-
-	/*
-	if files, err := ioutil.ReadDir(root); err == nil {
-		for _, file := range files {
-			if !file.IsDir() {
-				html, err = html.ParseFiles(filepath.Join(root, file.Name()))
-
-				if err != nil {
-					log.Panicln(err)
-				}
-
-				found = true
-			}
-		}
-	} else {
-		log.Println(err)
-	}
-
-	if !found {
-		err := fmt.Errorf ("no templates found in folder %v", root)
-		log.Fatalln(err)
-	}*/
-
+	
 	return html
 }
